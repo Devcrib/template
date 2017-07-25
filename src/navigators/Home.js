5dff0fb0d097fed7f565f81f5ca4c8ddf89e03c1
victor-iyiola <javafolabi@gmail.com>
2017-07-25 14:32:20 +0100

    Update README.md

--- src/navigators/Home.js
+++ src/navigators/Home.js
@@ -8,10 +8,12 @@ class Home extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up 'src/navigators/Home.js' to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Button title="Custom Button" onPress={() => Alert.alert('It worked!')} />
+        <Text>To add a new navigation item simply create a new file.</Text>
+        <Text>Export the created file and include it in 'src/navigators/index.js'</Text>
+        <Button title='Custom Button :)' onPress={() => Alert.alert('Yay!')} />
       </View>
     );
   }
